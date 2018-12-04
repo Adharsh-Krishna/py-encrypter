58e5b7419893f71d0c276a491624e1f4c5a600bf
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2018-12-05 00:17:33 +0530

    [Adharsh] Adds method to create user credential and modifies code

--- src/services/main.py
+++ src/services/main.py
@@ -7,26 +7,36 @@ class DataService:
     user_credential = None
 
     def __init__(self):
-        self.user = User
+        self.user = UserDetail
         self.user_credential = UserCredential
 
     def get_user_by_email(self, email):
         users = self.user.objects(email=email)
         return users[0] if len(users) > 0 else None
 
-    def get_user_credential_by_user_id(self, user_id):
+    def get_user_by_user_name(self, user_name):
+        user_credentials = self.user_credential.objects(user_name= user_name)
+        return user_credentials[0] if len(user_credentials) > 0 else None
+
+    def get_user_by_user_id(self, user_id):
         user_credentials = self.user_credential.objects(user=user_id)
         return user_credentials[0] if len(user_credentials) > 0 else None
 
-    def check_credentials(self, entered_email, entered_password):
-        desired_user = self.get_user_by_email(entered_email)
+    def check_credentials(self, user_name, entered_password):
+        desired_user = self.get_user_by_user_name(user_name)
         if desired_user is None:
             return False
-        desired_user_credential = self.get_user_credential_by_user_id(desired_user.id)
-        if bcrypt.checkpw(entered_password.encode('utf8'), desired_user_credential.password.encode('utf8')):
+        if bcrypt.checkpw(entered_password.encode('utf8'), desired_user.password.encode('utf8')):
             return True
         else:
             return False
+
+    @staticmethod
+    def create_user_credential(user_name, password):
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        new_user_credential = UserCredential(user_name=user_name, password=hashed_password)
+        new_user_credential.save()
+        print "created ..."
 
     # def create_user(self, e, f, l, p):
     #     u = User(email=e, first_name=f, last_name=l)
