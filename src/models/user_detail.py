264e1d58d2972b9a71a333834e6967ad09e60dbd
Adharsh Krishna <adharsh.rajan@tatacommunications.com>
2018-12-05 00:14:15 +0530

    [Adharsh] Renames user model to user detail model and adds reference to user credential model

--- src/models/user_detail.py
+++ src/models/user_detail.py
@@ -1,10 +1,12 @@
-from mongoengine import StringField, Document
+from src.models.user_credential import *
 from src.db.db import *
+from mongoengine import StringField, Document, ReferenceField
 
 Mongo.connect()
 
 
-class User(Document):
+class UserDetail(Document):
     email = StringField(required=True, max_length=100)
     first_name = StringField(required=True, max_length=50)
     last_name = StringField(required=True, max_length=50)
+    user = ReferenceField(UserCredential)
